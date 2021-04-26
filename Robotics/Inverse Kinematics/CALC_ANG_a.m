d319f0be89d6e452b9722794af86e567c13d3d57
Pierre <pierre.tabet@me.com>
2021-04-26 23:18:18 +0400

    Handwriting data
    Handwriting data

--- Robotics/Inverse Kinematics/CALC_ANG_a.m
+++
@@ -1,7 +0,0 @@
-function [angle_a] = CALC_ANG_a(x,y,length_a,length_b,angle_b)
-%CALC_ANG_a Preforms inverse kinematics
-
-angle_a = (atan(y/x)) + (atan((length_b * sin(angle_b))/(length_a + (length_b*cos(angle_b)))));
-
-
-end
--- Robotics/Inverse Kinematics/JOINT_POS.m
+++
@@ -1,9 +0,0 @@
-function [B_x,B_y] = JOINT_POS(length_a,angle_a)
-%JOINT_POS Calculates position of joints 2 and 4
-
-B_x = length_a * cos(angle_a);
-B_y = length_a * sin(angle_a);
-
-
-end
-
